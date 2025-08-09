a8cd51ccfc7ca67eade96d2a960fdb9f3ee5b104
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2025-08-09 18:49:04 +0100

    implement IsActive filter with tests

--- UserManagement.Services.Tests/UserServiceTests.cs
+++ UserManagement.Services.Tests/UserServiceTests.cs
@@ -11,16 +11,44 @@ public class UserServiceTests
     {
         // Arrange: Initializes objects and sets the value of the data that is passed to the method under test.
         var service = CreateService();
-        var users = SetupUsers();
+        var users = SetupSingleUser();
 
         // Act: Invokes the method under test with the arranged parameters.
         var result = service.GetAll();
 
         // Assert: Verifies that the action of the method under test behaves as expected.
-        result.Should().BeSameAs(users);
+        result.Should().BeEquivalentTo(users);
     }
 
-    private IQueryable<User> SetupUsers(string forename = "Johnny", string surname = "User", string email = "juser@example.com", bool isActive = true)
+    [Fact]
+    public void FilterByActive_IsTrue_WhenContextReturnsEntities_MustReturnActiveEntities()
+    {
+        // Arrange: Initializes objects and sets the value of the data that is passed to the method under test.
+        var service = CreateService();
+        SetupMultipleUsers([..ActiveUsers, ..InactiveUsers]);
+
+        // Act: Invokes the method under test with the arranged parameters.
+        var result = service.FilterByActive(true);
+
+        // Assert: Verifies that the action of the method under test behaves as expected.
+        result.Should().BeEquivalentTo(ActiveUsers);
+    }
+
+    [Fact]
+    public void FilterByActive_IsFalse_WhenContextReturnsEntities_MustReturnInactiveEntities()
+    {
+        // Arrange: Initializes objects and sets the value of the data that is passed to the method under test.
+        var service = CreateService();
+        SetupMultipleUsers([..ActiveUsers, ..InactiveUsers]);
+
+        // Act: Invokes the method under test with the arranged parameters.
+        var result = service.FilterByActive(false);
+
+        // Assert: Verifies that the action of the method under test behaves as expected.
+        result.Should().BeEquivalentTo(InactiveUsers);
+    }
+
+    private IQueryable<User> SetupSingleUser(string forename = "Johnny", string surname = "User", string email = "juser@example.com", bool isActive = true)
     {
         var users = new[]
         {
@@ -40,6 +68,27 @@ public class UserServiceTests
         return users;
     }
 
+    private IQueryable<User> SetupMultipleUsers(params User[] users)
+    {
+        var queryableUsers = users.AsQueryable();
+        _dataContext
+            .Setup(s => s.GetAll<User>())
+            .Returns(queryableUsers);
+
+        return queryableUsers;
+    }
+
     private readonly Mock<IDataContext> _dataContext = new();
     private UserService CreateService() => new(_dataContext.Object);
+
+    private static readonly User[] ActiveUsers =
+    [
+        new User { Forename = "Stuart", Surname = "Braithwaite", Email = "sBraithwaite@example.org", IsActive = true, },
+        new User { Forename = "Hazel ", Surname = "Wilde", Email = "hWilde@example.org", IsActive = true, },
+    ];
+
+    private static readonly User[] InactiveUsers =
+    [
+        new User { Forename = "Kurt", Surname = "Cobain", Email = "kCobain@example.org", IsActive = false, },
+    ];
 }
--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -4,28 +4,27 @@ using UserManagement.Web.Models.Users;
 
 namespace UserManagement.WebMS.Controllers;
 
-[Route("users")]
+[Route("[controller]")]
 public class UsersController : Controller
 {
     private readonly IUserService _userService;
     public UsersController(IUserService userService) => _userService = userService;
 
     [HttpGet]
-    public ViewResult List()
+    public ViewResult List(bool? isActive = null)
     {
-        var items = _userService.GetAll().Select(p => new UserListItemViewModel
+        var users = isActive is null ? _userService.GetAll() : _userService.FilterByActive(isActive.Value);
+
+        var items = users.Select(p => new UserListItemViewModel
         {
-            Id = p.Id,
+            Id       = p.Id,
             Forename = p.Forename,
-            Surname = p.Surname,
-            Email = p.Email,
+            Surname  = p.Surname,
+            Email    = p.Email,
             IsActive = p.IsActive
         });
 
-        var model = new UserListViewModel
-        {
-            Items = items.ToList()
-        };
+        var model = new UserListViewModel { Items = items.ToList() };
 
         return View(model);
     }
